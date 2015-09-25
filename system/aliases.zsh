5c258124a6a4ecb4bf29be788cdde8dd9efa7d90
Daniel Lucas <daniel.chris.lucas@gmail.com>
2015-09-25 15:25:32 +0200

    Adding gls

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,6 +1,16 @@
 if [ "$(uname -s)" != "Darwin" ]
 then
-    alias ls="ls -lah -F --color"
     alias pbcopy='xclip -selection clipboard'
     alias pbpaste='xclip -selection clipboard -o'
+fi
+
+# grc overides for ls
+# Made possible through contributions from generous benefactors like
+# `brew install coreutils`
+if $(gls &>/dev/null)
+then
+#     alias ls="gls -F --color=auto"
+    alias l="gls -lAhG"
+    alias ll="gls -lG"
+    alias la='gls -AG'
 fi
